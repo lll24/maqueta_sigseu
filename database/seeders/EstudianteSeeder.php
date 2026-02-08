1305c30e561d2b9a35ebe891e4b66c85def26cd3
lll24 <juandavidlongart@gmail.com>
2026-02-08 10:45:18 -0400

    Corrección de duplicados en EstudianteSeeder y ajustes locales

--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -17,7 +17,6 @@ class EstudianteSeeder extends Seeder
             ['cedula' => '31882343', 'nombre' => 'Juan David', 'apellido' => 'Longart', 'nacimiento' => '2005-02-10'],
             ['cedula' => '24482932', 'nombre' => 'Jose', 'apellido' => 'Miserol', 'nacimiento' => '2005-02-10'],
             ['cedula' => '31317970', 'nombre' => 'Adrián', 'apellido' => 'Reina', 'nacimiento' => '2003-01-25'],
-            ['cedula' => '24482932', 'nombre' => 'Jose', 'apellido' => 'Miserol', 'nacimiento' => '2005-02-10'],
             ['cedula' => '31882367', 'nombre' => 'Rafael', 'apellido' => 'Rodriguez', 'nacimiento' => '2005-02-10'],
             // Agrega aquí los 2 restantes del anteproyecto
         ];
