46b5626ca5d22be9dd404c34432f0410e0082d6a
Longart juan <juandavidlongart@gmail.com>
2026-06-26 12:59:15 -0400

    revisando

--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,5 @@
 <?php
-
+#pruebaaaa
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
